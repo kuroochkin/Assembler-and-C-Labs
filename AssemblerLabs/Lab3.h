a360b2943c190410f68adfd00b5b40bdb751471a
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-12 22:56:10 +0300

    Реализованы алгебраические операции

--- AssemblerLabs/Lab3.h
+++ AssemblerLabs/Lab3.h
@@ -8,30 +8,65 @@
 
 using namespace std;
 
+void PrintInfo(int p)
+{
+    cout << "Unsigned hexdecimal: 0x" << hex << p << endl; //16
+    cout << "Unsigned binary: " << bitset<16>(p) << endl; //2
+    cout << "Unsigned decimal: " << dec << p << endl; // 10
+    cout << "Signed hexdecimal: 0x" << hex << p << endl; //16
+    cout << "Signed binary: " << bitset<16>(p) << endl; //2
+    cout << "Signed decimal: " << dec << p << endl << endl; //10
+}
+
 void From16bitIn32bit(int16_t p)
 {
-    if(p < 0)
-    {
-        // Расширяем знаковое число до 32 бит
-        int32_t signed_extended = static_cast<int32_t>(p);
-
-        cout << "Unsigned hexdecimal: 0x" << hex << signed_extended << endl; //16
-        cout << "Unsigned binary: " << bitset<16>(signed_extended) << endl; //2
-        cout << "Unsigned decimal: " << dec << signed_extended << endl; // 10
-        cout << "Signed hexdecimal: 0x" << hex << signed_extended << endl; //16
-        cout << "Signed binary: " << bitset<16>(signed_extended) << endl; //2
-        cout << "Signed decimal: " << dec << signed_extended << endl << endl; //10
-    }
-    else
-    {
-        // Расширяем беззнаковое число до 32 бит
-        uint32_t unsigned_extended = static_cast<uint32_t>(p);
-        
-        cout << "Unsigned hexdecimal: 0x" << hex << unsigned_extended << endl; //16
-        cout << "Unsigned binary: " << bitset<16>(unsigned_extended) << endl; //2
-        cout << "Unsigned decimal: " << dec << unsigned_extended << endl; // 10
-        cout << "Signed hexdecimal: 0x" << hex << unsigned_extended << endl; //16
-        cout << "Signed binary: " << bitset<16>(unsigned_extended) << endl; //2
-        cout << "Signed decimal: " << dec << unsigned_extended << endl << endl; //10
-    }
+    // Расширяем знаковое число до 32 бит
+    int32_t signed_extended = static_cast<int32_t>(p);
+    PrintInfo(signed_extended);  
+    
+    // Расширяем беззнаковое число до 32 бит
+    uint32_t unsigned_extended = static_cast<uint32_t>(p);
+    PrintInfo(unsigned_extended);
 }
+
+void AlgebraicOperations(int16_t p)
+{
+    cout << "Число " << p << endl;
+
+    int16_t p1 = p * 2;
+    cout << "Беззнаковое умножение на 2: " << p1 << endl;
+
+    int16_t p2 = p / 2;
+    cout << "Беззнаковое деление на 2: " << p2 << endl;
+
+    int16_t p3 = p % 16;
+    cout << "Беззнаковое деление по остатку 16: " << p3 << endl;
+
+    p -= p % 16;
+    cout << "Округление вниз до числа, кратного 16: " << p << endl << endl;
+}
+
+void _AlgebraicOperations(uint16_t p)
+{
+    cout << "Число " << p << endl;
+
+    uint16_t _p1 = p * 2;
+    cout << "Знаковое умножение на 2: " << _p1 << endl;
+
+    uint16_t _p2 = p / 2;
+    cout << "Знаковое деление на 2: " << _p2 << endl;
+
+    uint16_t _p3 = p % 16;
+    cout << "Знаковое деление по остатку 16: " << _p3 << endl << endl; 
+}
+
+void ShiftBits(short x)
+{
+
+}
+
+void _ShiftBits(short x)
+{
+
+}
+
--- AssemblerLabs/main.cpp
+++ AssemblerLabs/main.cpp
@@ -9,8 +9,22 @@ int main()
     int16_t signed_num = -567;
     uint16_t unsigned_num = 123;
 
+#pragma region Task2
+    //ÇÀÄÀÍÈÅ 2
+    cout << "Task 2" << endl << endl;
+
     From16bitIn32bit(signed_num);
     From16bitIn32bit(unsigned_num);
+#pragma endregion
+
+#pragma region Task3
+    //ÇÀÄÀÍÈÅ 3
+    cout << "Task 3" << endl << endl;
+
+    _AlgebraicOperations(unsigned_num);
+    AlgebraicOperations(signed_num);
+#pragma endregion
+
 
     return 0;
 }
